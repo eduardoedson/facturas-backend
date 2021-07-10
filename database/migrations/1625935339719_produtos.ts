d4b5670a2abe5f004eb820c6fce8098a61509c96
Eduardo Edson Batista Cordeiro Alves <edu-23@hotmail.com>
2021-07-10 13:46:33 -0300

    migrate produto

---
+++ database/migrations/1625935339719_produtos.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Produtos extends BaseSchema {
+  protected tableName = 'produtos'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('nome').notNullable()
+      table.string('codigo').unique().notNullable()
+      table.float('valor').notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
