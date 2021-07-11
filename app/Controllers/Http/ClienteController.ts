fd47dfae4be34f66062b22a27a5ee94e7bd48706
Eduardo Edson Batista Cordeiro Alves <eduardo@MacBook-Air-de-Eduardo.local>
2021-07-11 20:44:58 -0300

    CRUD cliente

---
+++ app/Controllers/Http/ClienteController.ts
@@ -0,0 +1,39 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Cliente from 'App/Models/Cliente'
+
+export default class ClienteController {
+
+  public async create ({ request }: HttpContextContract) {
+    const { codigo, nome } = request.only(['codigo', 'nome'])
+    const cliente = await Cliente.create({ codigo, nome })
+    return cliente
+  }
+
+  public async list () {
+    return await Cliente.all()
+  }
+
+  public async get ({ request }: HttpContextContract) {
+    const { id } = request.params()
+    return await Cliente.find(id)
+  }
+
+  public async update ({ request }: HttpContextContract) {
+    const { id } = request.params()
+    const { codigo, nome } = request.only(['codigo', 'nome'])
+
+    const cliente = await Cliente.find(id)
+    cliente?.merge({ codigo, nome })
+    cliente?.save()
+    
+    return cliente
+  }
+
+  public async delete ({ request }: HttpContextContract) {
+    const { id } = request.params()
+    const cliente = await Cliente.find(id)
+    await cliente?.delete()
+
+    return true
+  }
+}
